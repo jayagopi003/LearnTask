f919f7e65d7531dd380bff4cf7446103e28e9179
jayagopi003 <jayagopi515@gmail.com>
2023-01-22 20:42:00 +0530

    New code merging

--- src/org/actions/actionTask.java
+++ src/org/actions/actionTask.java
@@ -15,14 +15,5 @@ public class actionTask {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\DELL\\eclipse-workspace\\LearnTask\\Driver\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		
-		Scanner s = new Scanner(System.in);
-		
-		System.out.println("Enter a Number");
-		int n = s.nextInt();
-		if(n%2==0) {
-			System.out.println("The number is even");
-		}else {
-			System.out.println("The number is Odd");
-		}
 	}
 }
